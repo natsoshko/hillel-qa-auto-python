7b9d09b7237b854b01e7988cd5c1d9a4bbc218cd
natsoshko <snattalysh@gmail.com>
2026-07-09 13:07:29 +0300

    homework_11 is added

---
+++ lesson_11/homework_11.py
@@ -0,0 +1,15 @@
+s_array = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+
+def func_sum_array_numbers(s_list):
+    array_split = s_list.split(",")
+    sum_ = 0
+    try:
+        for num in array_split:
+            sum_ += int(num)
+    except ValueError:
+        return "Can't do this - not only numbers here!"
+    return sum_
+
+for item in s_array:
+    # print(item)
+    print("sum of elements", item, "=", func_sum_array_numbers(item))
